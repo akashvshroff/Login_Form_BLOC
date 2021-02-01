8a2ed428f15676e5f8f7d6a86e8d2d32c0ca376e
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-02-01 13:10:37 +0530

    Added fields to login screen

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -5,7 +5,30 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20.0),
-      child: Column(),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          labelText: 'Email Address', hintText: 'you@example.com'),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
     );
   }
 }
