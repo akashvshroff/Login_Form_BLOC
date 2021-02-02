b0fb2e534543926ce1226cad8a80e932f947cf04
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-02-02 11:30:00 +0530

    BLOC API

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,6 +1,14 @@
 import 'dart:async';
 
 class Bloc {
-  final emailController = StreamController<String>();
-  final passwordController = StreamController<String>();
+  final _emailController = StreamController<String>();
+  final _passwordController = StreamController<String>();
+
+  //add data to the Streams
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  //retrieve Streams
+  Stream<String> get email => _emailController.stream;
+  Stream<String> get password => _passwordController.stream;
 }
