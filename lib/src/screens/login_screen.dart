2013b5d81515c1c3c27710c2b073b0d8aa0b521f
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-02-03 13:16:16 +0530

    StreamBuilder implementation

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -20,21 +20,35 @@ class LoginScreen extends StatelessWidget {
   }
 
   Widget emailField() {
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-          labelText: 'Email Address', hintText: 'you@example.com'),
-    );
+    return StreamBuilder(
+        stream: bloc.email,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changeEmail,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: 'Email Address',
+              hintText: 'you@example.com',
+              errorText: snapshot.error,
+            ),
+          );
+        });
   }
 
   Widget passwordField() {
-    return TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        labelText: 'Password',
-        hintText: 'Password',
-      ),
-    );
+    return StreamBuilder(
+        stream: bloc.password,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changePassword,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              hintText: 'Password',
+              errorText: snapshot.error,
+            ),
+          );
+        });
   }
 
   Widget submitButton() {
